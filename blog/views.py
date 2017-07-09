31f9002e9b2a03b439dd9965fafc0a7cdd207d32
sjattwood <sjattwood@gmail.com>
2017-07-09 16:10:59 -0700

    added detail views

--- blog/views.py
+++ blog/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from django.utils import timezone
 from .models import Postimade
+from django.shortcuts import render, get_object_or_404
 
 
 # Create your views here.
 def post_list(request):
 	posts = Postimade.objects.filter(published_date__lte=timezone.now()).order_by('published_date')
 	return render(request, 'blog/post_list.html', {'posts': posts})
+
+def post_detail(request, pk):
+  post = get_object_or_404(Postimade, pk=pk)
+  return render(request, 'blog/post_detail.html', {'post': post})
